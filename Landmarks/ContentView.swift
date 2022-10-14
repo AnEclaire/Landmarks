ce899ad3d093c7a1f4d5136a47d31d039ce055eb
Emma Gutierrez <eclaire.tv@gmail.com>
2022-10-13 23:09:58 -0700

    Finished first tutorial.

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,12 +10,37 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            MapView() // Imported MapView Obj
+                .ignoresSafeArea(edges: .top) // Extend to top of screen
+                .frame(height: 300)
+            
+            CircleImage() // Imported CircleImage Obj
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) { // Stack containing text
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            }
+            .padding() // Ensure content in stack is spaced from edge.
+            
+            Divider()
+            
+            Text("About Turtle Rock")
+                .font(.title2)
+            Text("A natural rock formation in the shape of a turtle.")
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            
+            Spacer() // Push all content in stack to top
         }
-        .padding()
     }
 }
 

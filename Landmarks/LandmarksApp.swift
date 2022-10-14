110c9b86161e8f98142b6fa0b7c20ca6b9920141
Emma Gutierrez <eclaire.tv@gmail.com>
2022-10-13 22:08:15 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Emma Gutierrez on 10/13/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
